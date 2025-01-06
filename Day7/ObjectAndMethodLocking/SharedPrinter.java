03c38269402ce1ba7bfb6f9ca2d26e3f00a54ba6
vasanth <vasanth15740@gmail.com>
2025-01-06 15:34:47 +0530

    Updated

--- Day7/ObjectAndMethodLocking/SharedPrinter.java
+++ Day7/ObjectAndMethodLocking/SharedPrinter.java
@@ -1,32 +1,86 @@
-package Day7.ObjectAndMethodLocking;
+//Object and method locking
+//Write a Java program to demonstrate object-level and method-level locking using multithreading. The program should:
+//Create a shared resource class (e.g., SharedPrinter) with two synchronized methods:
+//Method 1: Print a given string multiple times.
+//Method 2: Print a countdown from a given number.
+//Use two threads:
+//Thread 1: Calls the first synchronized method (print string).
+//Thread 2: Calls the second synchronized method (countdown).
+//Demonstrate the difference between:
+//Object-Level Locking: When both threads use the same instance of the shared resource.
+//Class-Level Locking: When both threads use a static synchronized method or block.
+//Show how the locks affect the execution order of the methods.
 
-public class SharedPrinter extends Thread{
-    synchronized void StringPrint(String str){
+package Day7.ObjectAndMethodLocking;
+class SharedPrinter {
+
+    public synchronized void printString(String str) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("Given String"+str);
+            System.out.println(str);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+
+            }
         }
-        try{
-            Thread.sleep(500);
-        }
-        catch (Exception e){
+    }
+    public synchronized void countdown(int num) {
+        for (int i = num; i > 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+
+            }
         }
     }
 
-    synchronized void CoutPrint(int count) {
-        for (int i = 0; i < count; i++) {
-            System.out.println("count down :" + i);
+    public static synchronized void staticPrintString(String str) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(str);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+
+            }
         }
-        try {
-            Thread.sleep(500);
-        } catch (Exception e) {
+    }
+    public static synchronized void staticCountdown(int num) {
+        for (int i = num; i > 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+
+            }
         }
     }
 
     public static void main(String[] args) {
-        SharedPrinter sp =new SharedPrinter();
-        Thread t1 = new Thread();
-        Thread t2 = new Thread();
-        t1.start();
-        t2.start();
+        SharedPrinter sharedPrinter = new SharedPrinter();
+        Thread thread1 = new Thread(() -> sharedPrinter.printString("Hello"));
+        Thread thread2 = new Thread(() -> sharedPrinter.countdown(5));
+        thread1.start();
+        thread2.start();
+        try {
+            thread2.join();
+            thread1.join();
+
+        } catch (Exception e) {
+
+        }
+        Thread thread3 = new Thread(() -> SharedPrinter.staticPrintString("World"));
+        Thread thread4 = new Thread(() -> SharedPrinter.staticCountdown(5));
+        thread3.start();
+        thread4.start();
+
+        try{
+            thread3.join();
+            thread4.join();
+        }
+
+        catch (Exception e){
+
+        }
     }
 }
