a8911255f7328b98ad83d0a5457163bbb8b710f8
vasanth <vasanth15740@gmail.com>
2025-01-17 08:56:58 +0530

    Updated

---
+++ Day13/ArraySorter.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArraySorter {
+
+    public int[] sortArray(int[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException("Array cannot be null");
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
